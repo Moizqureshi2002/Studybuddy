e7e808ae8b08016147e143f035e24f1bce1ebac0
Moizqureshi2002 <147783174+Moizqureshi2002@users.noreply.github.com>
2024-02-21 14:11:44 +0530

    Add files via upload

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
 
     'corsheaders.middleware.CorsMiddleware',
@@ -114,9 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # STATIC_ROOT = 
 
